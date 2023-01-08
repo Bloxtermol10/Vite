a2e72886d5febdebd54ccff185b7bd316b0e3723
Bloxtermol10 <alefrito554@gmail.com>
2023-01-08 14:47:36 -0500

    criptos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [cryptos, setCryptos] = useState();
+
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+      .then((response) => response.json())
+      .then((data) => setCryptos(data.data))
+      .catch(console.error("404 Data no foud"));
+  }, []);
+
+  
 
   return (
-    <h1>Hola bb</h1>
-  )
+    <>
+      <h1>Lista de crypto </h1>
+
+      <ol>
+        {cryptos ?
+          cryptos.map(({id, name, priceUsd}) => {
+            return (
+            <li key={id}>
+             Name: {name}   Price: {priceUsd}</li>
+            )
+          }):
+
+          <h1>Loading</h1>
+        }
+      </ol>
+    </>
+  );
 }
 
-export default App
+export default App;
